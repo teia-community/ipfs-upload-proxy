e380a4bbf85f6d1b8a5581f1c7e4602fd8b6b55b
Mel Massadian <melmassadian@gmail.com>
2022-08-19 16:46:53 +0200

    fix: ⚡️ add a "session" ID to the error

--- src/index.mjs
+++ src/index.mjs
@@ -6,6 +6,7 @@ import express from "express";
 import { Blob } from "buffer";
 import multer from "multer";
 import cors from "cors";
+import { nanoid } from "nanoid";
 
 const client = new NFTStorage({ token: process.env.API_TOKEN });
 const port = process.env.PORT || 4444;
@@ -22,15 +23,17 @@ const upload = multer({
   },
 });
 
-const handle_error = (res, req, error, status = 500) => {
-  console.error(`ERROR from ${req.hostname}@${req.ip} -> ${error}`);
-  res.status(status).send(error);
+const handle_error = (id, res, req, error, status = 500) => {
+  console.error(`ERROR from ${req.hostname}@${req.ip} | ${id} -> ${error}`);
+  res.status(status).send(`${id} | ${error}`);
 }
 
 app.post("/single", upload.single("asset"), async function (req, res) {
+
+  const id = nanoid()
   try {
     if (req.file == null) {
-      return handle_error(res, req, "Invalid request: 'file' is missing.", 400);
+      return handle_error(id, res, req, "Invalid request: 'file' is missing.", 400);
     }
 
     const { root, car } = await packToBlob({
@@ -43,14 +46,15 @@ app.post("/single", upload.single("asset"), async function (req, res) {
 
     res.json({ cid });
   } catch (err) {
-    handle_error(res, req, `unexpected error calling /single endpoint: ${err}`);
+    handle_error(id, res, req, `unexpected error calling /single endpoint: ${err}`);
   }
 });
 
 app.post("/multiple", upload.array("assets", 100), async function (req, res) {
+  const id = nanoid()
   try {
     if (req.files == null || req.files.length == 0) {
-      return handle_error(res, req, "Invalid request: 'files' is missing or empty.", 400);
+      return handle_error(id, res, req, "Invalid request: 'files' is missing or empty.", 400);
     }
     const cid = await client.storeDirectory(
       req.files.map((file) => new File([file.buffer], file.originalname))
@@ -58,7 +62,7 @@ app.post("/multiple", upload.array("assets", 100), async function (req, res) {
 
     res.json({ cid: cid.toString() });
   } catch (err) {
-    handle_error(req,res,`unexpected error calling /multiple endpoint: ${err}`);
+    handle_error(id, req, res, `unexpected error calling /multiple endpoint: ${err}`);
   }
 });
 
