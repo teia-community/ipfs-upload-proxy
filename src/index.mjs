430b7fa6ab162f3599601ab07b0434e76ecd668e
Simon Kusterer <simon@sope.io>
2022-09-03 13:08:40 +0200

    create id in handle_error function

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import { File, NFTStorage } from "nft.storage";
 import { packToBlob } from "ipfs-car/pack/blob";
-import { MemoryBlockStore } from "ipfs-car/blockstore/memory";
 import express from "express";
 import { Blob } from "buffer";
 import multer from "multer";
@@ -19,21 +18,20 @@ const upload = multer({
   storage,
   preservePath: true,
   limits: {
-    fileSize: 100000000, // 100MB
+    fileSize: process.env.FILE_SIZE_LIMIT || 100000000, // 100MB
   },
 });
 
-const handle_error = (id, res, req, error, status = 500) => {
+const handle_error = (res, req, error, status = 500) => {
+  const id = nanoid();
   console.error(`ERROR from ${req.hostname}@${req.ip} | ${id} -> ${error}`);
   res.status(status).send(`${id} | ${error}`);
-}
+};
 
 app.post("/single", upload.single("asset"), async function (req, res) {
-
-  const id = nanoid()
   try {
     if (req.file == null) {
-      return handle_error(id, res, req, "Invalid request: 'file' is missing.", 400);
+      return handle_error(res, req, "Invalid request: 'file' is missing.", 400);
     }
 
     const { root, car } = await packToBlob({
@@ -46,23 +44,20 @@ app.post("/single", upload.single("asset"), async function (req, res) {
 
     res.json({ cid });
   } catch (err) {
-    handle_error(id, res, req, `unexpected error calling /single endpoint: ${err}`);
+    handle_error(res, req, `unexpected error calling /single endpoint: ${err}`);
   }
 });
 
 app.post("/multiple", upload.array("assets", 100), async function (req, res) {
-  const id = nanoid()
   try {
     if (req.files == null || req.files.length == 0) {
-      return handle_error(id, res, req, "Invalid request: 'files' is missing or empty.", 400);
+      return handle_error(res, req, "Invalid request: 'files' is missing or empty.", 400);
     }
-    const cid = await client.storeDirectory(
-      req.files.map((file) => new File([file.buffer], file.originalname))
-    );
+    const cid = await client.storeDirectory(req.files.map((file) => new File([file.buffer], file.originalname)));
 
     res.json({ cid: cid.toString() });
   } catch (err) {
-    handle_error(id, req, res, `unexpected error calling /multiple endpoint: ${err}`);
+    handle_error(req, res, `unexpected error calling /multiple endpoint: ${err}`);
   }
 });
 
